6a4d328592b9b2b91e7aa546d6248aea3671d6b4
Esha Maharishi <esha.maharishi@gmail.com>
2025-11-03 21:18:52 -0500

    support joint to joint motion and vision services in arm position saver

--- touch/arm_position_saver.go
+++ touch/arm_position_saver.go
@@ -10,9 +10,11 @@ import (
 	"go.viam.com/rdk/components/arm"
 	toggleswitch "go.viam.com/rdk/components/switch"
 	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/motionplan/armplanning"
 	"go.viam.com/rdk/referenceframe"
 	"go.viam.com/rdk/resource"
 	"go.viam.com/rdk/services/motion"
+	"go.viam.com/rdk/services/vision"
 	"go.viam.com/rdk/spatialmath"
 	"go.viam.com/rdk/utils"
 
@@ -31,12 +33,13 @@ func init() {
 }
 
 type ArmPositionSaverConfig struct {
-	Arm         string
-	Joints      []float64
-	Motion      string
-	Point       r3.Vector
-	Orientation spatialmath.OrientationVectorDegrees
-	Extra       map[string]interface{}
+	Arm            string                               `json:"arm,omitempty"`
+	Joints         []float64                            `json:"joints,omitempty"`
+	Motion         string                               `json:"motion,omitempty"`
+	Point          r3.Vector                            `json:"point,omitzero"`
+	Orientation    spatialmath.OrientationVectorDegrees `json:"orientation,omitzero"`
+	VisionServices []string                             `json:"vision_services,omitempty"`
+	Extra          map[string]interface{}               `json:"extra,omitempty"`
 }
 
 func (c *ArmPositionSaverConfig) Validate(path string) ([]string, []string, error) {
@@ -52,6 +55,10 @@ func (c *ArmPositionSaverConfig) Validate(path string) ([]string, []string, erro
 		} else {
 			deps = append(deps, c.Motion)
 		}
+	}
+
+	if len(c.VisionServices) > 0 {
+		deps = append(deps, c.VisionServices...)
 	}
 
 	return deps, nil, nil
@@ -82,6 +89,16 @@ func newArmPositionSaver(ctx context.Context, deps resource.Dependencies, config
 		}
 	}
 
+	if len(newConf.VisionServices) > 0 {
+		for _, name := range newConf.VisionServices {
+			v, err := vision.FromProvider(deps, name)
+			if err != nil {
+				return nil, err
+			}
+			aps.visionServices = append(aps.visionServices, v)
+		}
+	}
+
 	return aps, nil
 }
 
@@ -93,8 +110,9 @@ type ArmPositionSaver struct {
 	cfg    *ArmPositionSaverConfig
 	logger logging.Logger
 
-	arm    arm.Arm
-	motion motion.Service
+	arm            arm.Arm
+	motion         motion.Service
+	visionServices []vision.Service
 }
 
 func (aps *ArmPositionSaver) Name() resource.Name {
@@ -168,46 +186,85 @@ func (aps *ArmPositionSaver) saveCurrentPosition(ctx context.Context) error {
 }
 
 func (aps *ArmPositionSaver) goToSavePosition(ctx context.Context) error {
-	if len(aps.cfg.Joints) > 0 {
-		return aps.arm.MoveToJointPositions(ctx, aps.cfg.Joints, aps.cfg.Extra)
-	}
-
 	if aps.motion != nil {
-		current, err := aps.motion.GetPose(ctx, aps.cfg.Arm, "world", nil, nil)
+		// Add obstacles to the world state from the configured vision services
+		var obstacles []*referenceframe.GeometriesInFrame
+		for _, v := range aps.visionServices {
+			vizs, err := v.GetObjectPointClouds(ctx, "", nil)
+			if err != nil {
+				return fmt.Errorf("error while calling GetObjectPointClouds on vision service %s, %w", v.Name(), err)
+			}
+			for _, viz := range vizs {
+				if viz.Geometry != nil {
+					gif := referenceframe.NewGeometriesInFrame(referenceframe.World, []spatialmath.Geometry{viz.Geometry})
+					obstacles = append(obstacles, gif)
+				}
+			}
+		}
+		worldState, err := referenceframe.NewWorldState(obstacles, []*referenceframe.LinkInFrame{} /* no additional transforms */)
 		if err != nil {
-			return err
+			return fmt.Errorf("couldn't create world state: %w", err)
 		}
 
-		linearDelta := current.Pose().Point().Distance(aps.cfg.Point)
-		orientationDelta := spatialmath.QuatToR3AA(spatialmath.OrientationBetween(current.Pose().Orientation(), &aps.cfg.Orientation).Quaternion()).Norm2()
+		if len(aps.cfg.Joints) > 0 {
+			aps.logger.Debugf("using joint to joint motion")
 
-		aps.logger.Debugf("goToSavePosition linearDelta: %v orientationDelta: %v", linearDelta, orientationDelta)
-		if linearDelta < .1 && orientationDelta < .01 {
-			aps.logger.Debugf("close enough, not moving - linearDelta: %v orientationDelta: %v", linearDelta, orientationDelta)
+			// Express the goal state in joint positions
+			goalFrameSystemInputs := make(referenceframe.FrameSystemInputs)
+			goalFrameSystemInputs[aps.arm.Name().Name] = aps.cfg.Joints
+			extra := map[string]any{"goal_state": armplanning.NewPlanState(nil, goalFrameSystemInputs).Serialize()}
+
+			// Call Motion.Move
+			_, err = aps.motion.Move(ctx, motion.MoveReq{
+				ComponentName: aps.cfg.Arm,
+				WorldState:    worldState,
+				Extra:         extra,
+			})
+			return err
+		} else {
+			aps.logger.Debugf("using cartesian motion")
+
+			// Check if we are already close enough
+			current, err := aps.motion.GetPose(ctx, aps.cfg.Arm, "world", nil, nil)
+			if err != nil {
+				return err
+			}
+			linearDelta := current.Pose().Point().Distance(aps.cfg.Point)
+			orientationDelta := spatialmath.QuatToR3AA(spatialmath.OrientationBetween(current.Pose().Orientation(), &aps.cfg.Orientation).Quaternion()).Norm2()
+			aps.logger.Debugf("goToSavePosition linearDelta: %v orientationDelta: %v", linearDelta, orientationDelta)
+			if linearDelta < .1 && orientationDelta < .01 {
+				aps.logger.Debugf("close enough, not moving - linearDelta: %v orientationDelta: %v", linearDelta, orientationDelta)
+				return nil
+			}
+
+			// Express the goal state in cartesian pose
+			pif := referenceframe.NewPoseInFrame(
+				"world",
+				spatialmath.NewPose(aps.cfg.Point, &aps.cfg.Orientation),
+			)
+
+			// Call Motion.Move
+			done, err := aps.motion.Move(
+				ctx,
+				motion.MoveReq{
+					ComponentName: aps.cfg.Arm,
+					Destination:   pif,
+					WorldState:    worldState,
+					Extra:         aps.cfg.Extra,
+				},
+			)
+			if err != nil {
+				return err
+			}
+			if !done {
+				return fmt.Errorf("move didn't finish")
+			}
 			return nil
 		}
+	} else if len(aps.cfg.Joints) > 0 {
+		aps.logger.Debugf("using MoveToJointPositions")
 
-		pif := referenceframe.NewPoseInFrame(
-			"world",
-			spatialmath.NewPose(aps.cfg.Point, &aps.cfg.Orientation),
-		)
-
-		done, err := aps.motion.Move(
-			ctx,
-			motion.MoveReq{
-				ComponentName: aps.cfg.Arm,
-				Destination:   pif,
-				WorldState:    nil,
-				Extra:         aps.cfg.Extra,
-			},
-		)
-		if err != nil {
-			return err
-		}
-		if !done {
-			return fmt.Errorf("move didn't finish")
-		}
-		return nil
+		return aps.arm.MoveToJointPositions(ctx, aps.cfg.Joints, aps.cfg.Extra)
 	}
 
 	return fmt.Errorf("need to configure where to go")
